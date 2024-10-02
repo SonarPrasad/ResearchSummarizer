e7eb2711a97c7ed6173a23c128ac476cae0efe2b
SonarPrasad <prasadsonar09@gmail.com>
2024-10-02 21:54:38 +0530

    Updates for Deployment

--- ResearchSummarizer/settings.py
+++ ResearchSummarizer/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'researchsummarizer.onrender.com']
 
 
 # Application definition
